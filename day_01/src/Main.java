cf598e7d51b0e154c61f14395539712d3a380c2a
Mao <mao527147880@gmail.com>
2019-11-22 17:41:46 +0800

    万达影城--第一个人修改123

--- day_01/src/Main.java
+++ day_01/src/Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("欢迎观临 万达影音城----第二个人修改的");
+        System.out.println("欢迎观临 万达影音城111111");
         System.out.println("欢迎观临 万达影音城222222");
         System.out.println("欢迎观临 万达影音城333333");
         Scanner sc = new Scanner(System.in);
